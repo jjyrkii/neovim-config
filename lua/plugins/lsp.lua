a35966ca33772d338f26b8a5624cb2dbd31a739a
jyrki <jyrki1887@gmail.com>
2024-03-11 20:55:11 +0100

    tree added and some other stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -135,7 +135,7 @@ return {
       -- clangd = {},
       -- gopls = {},
       -- pyright = {},
-      -- rust_analyzer = {},
+      rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -144,6 +144,8 @@ return {
       -- But for many setups, the LSP (`tsserver`) will work just fine
       -- tsserver = {},
       --
+
+      ansiblels = {},
 
       lua_ls = {
         -- cmd = {...},
